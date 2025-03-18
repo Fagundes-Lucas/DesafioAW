109a7669f93400aabab80edf2fe63ea2649f9014
Lucas Fagundes <fagundess.lucas@gmail.com>
2025-03-18 19:46:38 +0000

    Teste de veracidade reescrito com boas praticas

--- tests/total_vendas_2011.sql
+++ tests/total_vendas_2011.sql
@@ -1,13 +1,12 @@
--- This test ensures that the gross sales for 2011 match the audited accounting value:
--- $12,646,112.16
+with
+    gross_value_2011 as(
+        select
+            sum(gross_value) as total_gross_value
+        from {{ ref('int_sales') }}
+        where year(order_date) = 2011
+    )
 
-WITH sales_in_2011 AS (
-    SELECT 
-        SUM(gross_value) AS total_gross_sales  -- Using the pre-defined metric
-    FROM {{ ref('int_sales') }}  -- Referencing the intermediate sales table
-    WHERE order_date BETWEEN '2011-01-01' AND '2011-12-31'
-)
-
-SELECT total_gross_sales
-FROM sales_in_2011
-WHERE total_gross_sales != 12646112.16  -- The test will fail if the value does not match
+select
+    total_gross_value
+from gross_value_2011
+where total_gross_value not between 12646112.16 and 12646112.26
